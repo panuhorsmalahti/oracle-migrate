8d54b1ef1af0d37b0f0c16d438351fbffc00c640
Oleh Chaplya <olehch7@gmail.com>
2016-09-22 20:02:39 +0300

    init

---
+++ migrations/sql/1474377231462-noname-up.sql
@@ -0,0 +1,7 @@
+create table emp (empno number(5) primary key,
+                   name varchar2(20),
+                   sal number(10,2),
+                   job varchar2(20),
+                   mgr  number(5),
+                   Hiredate  date,
+                   comm number(10,2))
---
+++ migrations/sql/1474378804724-app-table-down.sql
@@ -0,0 +1,1 @@
+alter table "USERS" drop column "XZVQWEE"
