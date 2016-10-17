5cd295940a285de02e4a89cc8b21fafae1bebc7f
Oleh Chaplya <olehch7@gmail.com>
2016-10-17 21:26:56 +0300

    Improved migration script template

--- templates/create.js
+++ templates/create.js
@@ -2,20 +2,14 @@ const migrate = require('oracle-migrate');
 
 /**
  * Migrates database up
- *
- * @param {Function} next callback
- * @returns {Promise} returns result of executed `up` migration
  */
-exports.up = function (next) {
+exports.up = function () {
   return migrate.readFile('{up}');
 };
 
 /**
  * Migrates database down
- *
- * @param {Function} next callback
- * @returns {Promise} returns result of executed `down` migration
  */
-exports.down = function (next) {
+exports.down = function () {
   return migrate.readFile('{down}');
 };
