812e2c48f9ff44d97cb2f0e2e22dd464d4e79507
Oleh Chaplya <olehch7@gmail.com>
2016-10-18 16:53:58 +0300

    Added possibility to use js module file as a config

--- templates/create.js
+++ templates/create.js
@@ -1,15 +1,9 @@
 const migrate = require('oracle-migrate');
 
-/**
- * Migrates database up
- */
 exports.up = function () {
   return migrate.readFile('{up}');
 };
 
-/**
- * Migrates database down
- */
 exports.down = function () {
   return migrate.readFile('{down}');
 };
