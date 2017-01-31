eadfe5d7ed5578bcedf4c39dc98dbbbe88826ae4
Panu Horsmalahti <panu.horsmalahti@digia.com>
2017-01-31 11:35:32 +0200

    Expose the API for programmatic usage.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const tools = require('./lib/tools');
+const libCommands = require('./lib/commands');
 
 module.exports = {
-  readFile: tools.readFile
+  readFile: tools.readFile,
+  tools: tools,
+  libCommands: libCommands
 };
